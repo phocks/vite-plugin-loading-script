01097057e7e60a4942f0cf564b866ec7796b29bf
Joshua Byrd <phocks@gmail.com>
2025-12-04 17:11:07 +1000

    Separate functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import sum from "hash-sum";
 import { PluginOption, ViteDevServer } from "vite";
-import { OutputBundle, OutputChunk } from "rollup";
+import { OutputBundle } from "rollup";
+import { generateLoadingScript } from "./lib";
 
 interface LoadingScriptOptions {
   externalSrc?: string;
@@ -62,55 +63,3 @@ export function loadingScript({
     },
   };
 }
-
-const generateLoadingScript = (
-  bundle: OutputBundle,
-  externalSource?: string,
-  crossorigin?: boolean,
-  crossoriginVal?: string
-): string => {
-  let scriptCode = `
-(function () {
-  var scriptTag = document.currentScript;
-  var src = scriptTag.src;
-  var basePath = src.substring(0, src.lastIndexOf('/') + 1);
-  var parent = scriptTag.parentNode;
-`;
-  let counter = 0;
-  for (const key in bundle) {
-    // TODO: External source might not make sense any more considering
-    // detection of currentScript src and basePath. Maybe remove.
-    const filename = externalSource
-      ? externalSource + bundle[key].fileName
-      : bundle[key].fileName;
-    const varName = `file${counter}`;
-    if (bundle[key].type === "chunk") {
-      const chunk = bundle[key] as OutputChunk;
-      if (chunk.isEntry) {
-        scriptCode += `const ${varName} = document.createElement('script');`;
-        scriptCode += `${varName}.setAttribute('src', basePath + '${filename}');`;
-        scriptCode += `${varName}.setAttribute('type', 'module');`;
-        if (crossorigin) {
-          scriptCode += `${varName}.setAttribute('crossorigin', '${crossoriginVal}');`;
-        }
-      } else {
-        scriptCode += `const ${varName} = document.createElement('link');`;
-        scriptCode += `${varName}.setAttribute('href', basePath + '${filename}');`;
-        scriptCode += `${varName}.setAttribute('rel', 'modulepreload');`;
-      }
-    } else if (
-      bundle[key].type === "asset" &&
-      bundle[key].fileName.endsWith(".css")
-    ) {
-      scriptCode += `const ${varName} = document.createElement('link');`;
-      scriptCode += `${varName}.setAttribute('href', basePath + '${filename}');`;
-      scriptCode += `${varName}.setAttribute('rel', 'stylesheet');`;
-    } else {
-      continue;
-    }
-    scriptCode += `parent.appendChild(${varName});`;
-    counter++;
-  }
-  scriptCode += "})()";
-  return scriptCode;
-};
